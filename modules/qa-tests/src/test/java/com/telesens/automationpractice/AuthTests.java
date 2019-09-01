44f7061f99351bc72ebe75e38fcf9934803d39c2
Helga22 <symbols22@gmail.com>
2019-09-01 23:00:28 +0300

    first commit

---
+++ modules/qa-tests/src/test/java/com/telesens/automationpractice/AuthTests.java
@@ -0,0 +1,46 @@
+package com.telesens.automationpractice;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AuthTests {
+    private WebDriver driver;
+    private String baseUrl = "http://automationpractice.com/index.php";
+    private String login = System.getProperty("login");
+    private String password = System.getProperty("password");
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "D:/drivers/selenium/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testAuthSuccess() throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys(login);
+        driver.findElement(By.id("passwd")).click();
+        driver.findElement(By.id("passwd")).clear();
+        driver.findElement(By.id("passwd")).sendKeys(password);
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forgot your password?'])[1]/following::span[1]")).click();
+        driver.findElement(By.linkText("Sign out")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
